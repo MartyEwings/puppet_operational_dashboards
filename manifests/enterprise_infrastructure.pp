419ad6a8a0fc8a960cdc809f6258c0dc9877da6c
martyewings <martyewings@gmail.com>
2022-04-26 13:53:48 +0100

    install class for ent infrastructure agents

---
+++ manifests/enterprise_infrastructure.pp
@@ -0,0 +1,18 @@
+# @summary Installs dependancies for Operational dashboards on PE infrastructure components
+#
+# When applied to an appropriate node group this class applies the toml gem and database access
+# On appropriate infrastructure nodes in PE
+#
+# @example
+#   include puppet_operational_dashboards::enterprise_infrastructure
+# @param profiles
+#   Array of PE profiles on the node with this class applied. 
+class puppet_operational_dashboards::enterprise_infrastructure (
+  Array[String] $profiles = puppet_operational_dashboards::pe_profiles_on_host(),
+) {
+  if  $profiles.match(/Master/) != 'undef' {
+    include influxdb::profile::toml
+  } elsif  $profiles.match(/Datasbase/) != 'undef' {
+    include puppet_operational_dashboards::profile::postgres_access
+  }
+}
